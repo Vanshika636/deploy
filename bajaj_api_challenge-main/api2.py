2334d7475c130629e856394b0d604e62525d78bb
Vanshika636 <106257775+Vanshika636@users.noreply.github.com>
2024-03-18 14:30:13 +0530

    Create api2.py

---
+++ bajaj_api_challenge-main/api2.py
@@ -0,0 +1,27 @@
+from flask import Flask,jsonify,request
+api_url = " https://customer-analytics-34146.my.salesforce-sites.com/services/apexrest/buyStocks"
+app = Flask(__name__)
+
+@app.route('/customer-analytics-34146.my.salesforce-sites.com/services/apexrest/buyStocks', methods=['POST'])
+def welcome():
+    print(request.json["data"])
+    numbers = []
+    alphabets = []
+    for data in request.json["data"]:
+        if data.isalpha():
+            alphabets.append(data)
+        else:
+            numbers.append(data)
+    return jsonify({
+        "is_success": True,
+        "name": "john",
+        "email" : "john@xyz.com",
+        "roll_number":"ABCD123",
+        "phone" : "9996665050"
+        "numbers": numbers,
+        "alphabets": alphabets}
+                   
+    )
+
+if __name__ == '__main__':
+    app.run()
